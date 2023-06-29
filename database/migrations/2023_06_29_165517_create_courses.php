1d81695d83f180a1d18bd4bc7947cc253344bfec
ZepiDarmawanTambunan <zefidarmawan@gmail.com>
2023-06-30 00:06:18 +0700

    add migration

---
+++ database/migrations/2023_06_29_165517_create_courses.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->boolean('certificate');
+            $table->string('thumbnail')->nullable();
+            $table->enum('type', ['free', 'premium']);
+            $table->integer('price')->unsigned()->default(0);
+            $table->enum('level', ['all-level', 'beginner', 'intermediate', 'advance']);
+            $table->longText('description')->nullable();
+            $table->foreignId('mentor_id')->constrained('mentors')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('courses');
+    }
+};
